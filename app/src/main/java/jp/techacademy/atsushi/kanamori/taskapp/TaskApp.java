d78d0dfa25c178d1f3a28725c9346932cbf56f7f
kanamori <dorakuruse@yahoo.co.jp>
2017-06-11 18:39:09 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/atsushi/kanamori/taskapp/TaskApp.java
@@ -0,0 +1,13 @@
+package jp.techacademy.atsushi.kanamori.taskapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
